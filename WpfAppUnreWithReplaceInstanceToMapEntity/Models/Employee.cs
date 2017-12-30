dad4d840cd4b306522184db5265edbe5a578406c
小杉 孝広 <temparamental@hotmail.com>
2017-12-30 17:22:47 +0900

    Add Map To ref T object/Instance Do/Undo/Redo result.

---
+++ WpfAppUnreWithReplaceInstanceToMapEntity/Models/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppUnreWithReplaceInstanceToMapEntity.Models
+{
+    public class Employee
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+
+        public int UserId { get; set; } = -1;
+
+        public string Name { get; set; } = "hogemoge";
+        public string Position { get; set; } = "hoge position";
+        public string Department { get; set; } = "moge department";
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static ObservableCollection<Employee> GetSamples()
+        {
+            ObservableCollection<Employee> stuff = new ObservableCollection<Employee>();
+            stuff.Add(new Employee() { Name = "Gregory S. Price", Department = "", Position = "President" });
+            stuff.Add(new Employee() { Name = "Irma R. Marshall", Department = "Marketing", Position = "Vice President" });
+            stuff.Add(new Employee() { Name = "John C. Powell", Department = "Operations", Position = "Vice President" });
+            stuff.Add(new Employee() { Name = "Christian P. Laclair", Department = "Production", Position = "Vice President" });
+            stuff.Add(new Employee() { Name = "Karen J. Kelly", Department = "Finance", Position = "Vice President" });
+
+            stuff.Add(new Employee() { Name = "Brian C. Cowling", Department = "Marketing", Position = "Manager" });
+            stuff.Add(new Employee() { Name = "Thomas C. Dawson", Department = "Marketing", Position = "Manager" });
+            stuff.Add(new Employee() { Name = "Angel M. Wilson", Department = "Marketing", Position = "Manager" });
+            stuff.Add(new Employee() { Name = "Bryan R. Henderson", Department = "Marketing", Position = "Manager" });
+
+            stuff.Add(new Employee() { Name = "Harold S. Brandes", Department = "Operations", Position = "Manager" });
+            stuff.Add(new Employee() { Name = "Michael S. Blevins", Department = "Operations", Position = "Manager" });
+            stuff.Add(new Employee() { Name = "Jan K. Sisk", Department = "Operations", Position = "Manager" });
+            stuff.Add(new Employee() { Name = "Sidney L. Holder", Department = "Operations", Position = "Manager" });
+
+            stuff.Add(new Employee() { Name = "James L. Kelsey", Department = "Production", Position = "Manager" });
+            stuff.Add(new Employee() { Name = "Howard M. Carpenter", Department = "Production", Position = "Manager" });
+            stuff.Add(new Employee() { Name = "Jennifer T. Tapia", Department = "Production", Position = "Manager" });
+
+            stuff.Add(new Employee() { Name = "Judith P. Underhill", Department = "Finance", Position = "Manager" });
+            stuff.Add(new Employee() { Name = "Russell E. Belton", Department = "Finance", Position = "Manager" });
+            return stuff;
+        }
+    }
+}
---
+++ WpfAppUnreWithReplaceInstanceToMapEntity/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Commands;
+using Prism.Mvvm;
+using Unre;
+using WpfAppUnreWithReplaceInstanceToMapEntity.Models;
+
+namespace WpfAppUnreWithReplaceInstanceToMapEntity.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private static MainWindowViewModel _myInstance = new MainWindowViewModel();
+
+        public static MainWindowViewModel Instance
+        {
+            get => _myInstance;
+            set => _myInstance = value;
+        }
+
+        public DelegateCommand DoCommand { get; set; }
+
+        public DelegateCommand UndoCommand { get; set; }
+
+        public DelegateCommand RedoCommand { get; set; }
+
+        public UnreRepository<Employee> Repository { get; set; } = new UnreRepository<Employee>();
+
+        private Employee _myCurrentEmployee = new Employee();
+
+        public Employee CurrentEmployee
+        {
+            get => _myCurrentEmployee;
+            set => SetProperty(ref _myCurrentEmployee, value);
+        }
+
+        private string _myStatus = "not do/undo/redo";
+
+        public string Status
+        {
+            get => _myStatus;
+            set => SetProperty(ref _myStatus, value);
+        }
+
+        private int _myCount = 0;
+        public MainWindowViewModel()
+        {
+            DoCommand = new DelegateCommand(() =>
+            {
+                var emp = new Employee();
+                emp.UserId = (_myCount += 1);
+                Repository.Do(ref _myCurrentEmployee, emp);
+                Status = CurrentEmployee.UserId.ToString();
+                RaiseEachCanExecute();
+            });
+
+            RedoCommand = new DelegateCommand(() =>
+            {
+                Repository.Redo(ref _myCurrentEmployee);
+                Status = CurrentEmployee.UserId.ToString();
+                RaiseEachCanExecute();
+            }, () => Repository.IsCanRedo);
+
+            UndoCommand = new DelegateCommand(() =>
+            {
+                Repository.Undo(ref _myCurrentEmployee);
+                Status = CurrentEmployee.UserId.ToString();
+                RaiseEachCanExecute();
+            }, () => Repository.IsCanUndo);
+        }
+
+        private void RaiseEachCanExecute()
+        {
+            UndoCommand?.RaiseCanExecuteChanged();
+            RedoCommand?.RaiseCanExecuteChanged();
+        }
+    }
+}
